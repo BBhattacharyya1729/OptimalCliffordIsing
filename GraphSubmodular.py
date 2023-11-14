90d5c3d66e3f0f9f98a565d1a753e51da33405da
BBhattacharyya1729 <bbhattacharyya@imsa.edu>
2023-11-14 03:13:21 +0000

    Added/Finalized comments

--- GraphSubmodular.py
+++ GraphSubmodular.py
@@ -10,8 +10,14 @@ from qiskit.quantum_info import Operator,Pauli,SparsePauliOp, Statevector
 
 ##GRAPH THEORETIC FUNCTIONS
 
-#Get all the edges from an adjaceny matrix as a list (of tuples)
 def getEdges(A):
+    '''
+    Get all the edges from an adjaceny matrix as a list (of tuples)
+    (np.array) A: Adjacency matrix of graph
+    
+    Returns:
+    (List[(Int,Int)]): List of all edges as tuples
+    '''
     D=[]
     for i in range(len(A)):
         for j in range(i+1,len(A)):
@@ -19,16 +25,31 @@ def getEdges(A):
                 D.append((i,j))
     return D
 
-#Get the adjaceny matrix from a list of edges (symmetric |V| x |V| matrix)
 def getMatrix(D,n):
+    '''
+    Get the adjaceny matrix from a list of edges (symmetric n x n matrix)
+    (List[(Int,Int)]) D: List of all edges as tuples
+    (Int) n: Number of verticies
+    
+    Returns:
+    (np.array): Adjacency matrix of graph
+    '''
     A=np.zeros((n,n),dtype='int')
     for i in D:
         A[i[0]][i[1]]=1
         A[i[1]][i[0]]=1
     return A 
 
-#Induced Subgraph from vertex set l
+
 def InducedSubgraph(A,l):
+    '''
+    Induced Subgraph from vertex set l
+    (np.array) A: Adjacency matrix of initial graph
+    (List[(Int,Int)]) l: List of all edges as tuples
+    
+    Returns:
+    (np.array): Adjacency matrix of induced graph
+    '''
     B = A.copy()
     for i in range(len(B)):
         if i not in l:
@@ -36,34 +57,70 @@ def InducedSubgraph(A,l):
                 B[i][k], B[k][i] = 0,0
     return B
 
-#Cost Function (is submodular for g>=0 as desired)
 def CostFunction(A,g):
+    '''
+    Cost Function (is submodular for g>=0 as desired)
+    (np.array) A: Adjacency matrix of graph
+    (Float) g: Magnetic parameter
+    
+    Returns:
+    (Function): Cost function
+    '''
     def Cost(l):
         return -(np.sum(InducedSubgraph(A,l))//2)+g*len(l)
     return Cost
 
-### Implents the Greedy - Wolfe Algorithm to minimize w^Tx for x in B_f
+ 
 def Greedy(f, w):
+    '''
+    Implents the Greedy - Wolfe Algorithm to minimize w^Tx for x in B_f
+    (List[Float]) w: weight vector
+    (Function) f: Submodular function
+    
+    Returns:
+    (List[Float]): Minimal point
+    '''
     z = list(zip(w,range(len(w))))
     z.sort()
     v = [i[1] for i in z]
     return VertexSet(f,v)
 
-###Obtains a vertex of B_f to start the Fujishige-Wolfe Algorithm (from the identity permutation)
+
 def Vertex(f,N):
+    '''
+    Obtains a vertex of B_f to start the Fujishige-Wolfe Algorithm (from the identity permutation)
+    (Function) f: Submodular function
+    (Int) N: Number of qubits
+    
+    Returns:
+    (List[Float]): B_f vertex 
+    '''
     return VertexSet(f,list(range(N)))
 
-###Get a vertex from a permutation of the numbers
 def VertexSet(f,l):
+    '''
+    Get a vertex from a permutation of the numbers
+    (Function) f: Submodular function
+    Float[Int]: Re-arrangement of vertex indicies
+    
+    Returns:
+    (List[Float]): B_f vertex 
+    '''
     x=[0]*len(l)
     for j in range(len(l)):
         x[l[j]]=(f(l[:j+1])-f(l[:j]))
     return x
 
 
-###Affine Minimizer
 #We handle the case for degenerate systems seperately (we generally don't see degeneracy though)
 def AffineMinimizer(S):
+    '''
+    Affine Minimizer Function
+    (List[List(Float)]): List of verticies to run the affine minimizer over
+    
+    Returns:
+    (List[Float],List[Float]): Point in the affine hull of S and associated coefficients
+    '''
     if(len(np.array(sym.Matrix(np.array(S).T).nullspace())) != 0):
         B=np.array(S).T
         v=np.array(sym.Matrix(B).nullspace(),dtype='float')[0]
@@ -76,9 +133,17 @@ def AffineMinimizer(S):
         alpha = M.dot(one)/(one.T.dot(M).dot(one))
         return np.reshape(B.dot(alpha),len(B.dot(alpha))),np.reshape(alpha,len(alpha))
     
-    
-#Fujishige-Wolfe Algorithm 
+     
 def SubmodularOpt(f,N,tol):
+    '''
+    Fujishige-Wolfe Algorithm
+    (Function) f: Submodular function
+    (Int) N: number of verticies
+    (Float) tol: Tolerance
+    
+    Returns:
+    (List[Float]): Minimum norm point in B_f
+    '''
     q=np.array(Vertex(f,N))  ##Initialization
     x=q
     S=[x]
@@ -107,8 +172,15 @@ def SubmodularOpt(f,N,tol):
                 l = [v for v in l if v>0]
     return x
 
-##Use a LP problem to check for the desnset subgraph
+
 def twoSegmentCheck(G):
+    '''
+    Use a LP to check for the desnset subgraph
+    (np.array) G: Adjacency Matrix
+    
+    Returns:
+    (Bool): Whether or not G is the densest subgraph
+    '''
     Edge_List = getEdges(G)
     c = [1]*len(Edge_List) + [0]*len(G)
 
@@ -140,12 +212,30 @@ def twoSegmentCheck(G):
 
 ### This section implements the specified Hamiltonian 
 
-#Obtain operator O_n
 def indexedOperator(O,n,numQubits):
+    '''
+    Obtain operator O_n
+    
+    (String) O: Label for operator as a string
+    (Int) n: Operator index
+    (Int) numQubits: number of qubits
+    
+    Returns:
+    (SparsePauliOp): Indexed operator O_n
+    '''
     return SparsePauliOp('I'*(numQubits - n - 1) + O + 'I'*n)
 
-#Get the Hamiltonian for Graph A and transvesre field g
+
 def getIsingHamiltonian(A,g):
+    '''
+    Get the Hamiltonian for Graph A and transvesre field g
+    
+    (np.array) A: Adjacency matrix
+    (Float) g: Magnetic parameter
+    
+    Returns:
+    (SparsePauliOp): Specified Hamiltonian
+    '''
     numQubits = len(A)
     O=0*SparsePauliOp('I'*numQubits)
     for i in range(numQubits):
@@ -155,9 +245,16 @@ def getIsingHamiltonian(A,g):
     return O.simplify()
 
 
-###Generate the data for a graph and a set of g - values
+
 def generateData(G,g_values):
+    '''
+    Generate the data for a graph and a set of g - values
+    (np.array) G: Adjacency list
+    (Iterable[Float]) g_values: Collection of g values
     
+    Returns:
+    (List[Float],List[Float],List[np.array]): List of computed approximations, List of exact eigenvalues, List of corresponding subgraphs (as adjacency matricies)
+    '''
     two_segmented = twoSegmentCheck(G)
     if(not two_segmented):
         graph_data = []
